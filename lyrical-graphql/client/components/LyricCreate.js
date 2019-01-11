68aa01e6ab1e7bdc2a1d8105c1cb89ba34d9b7ea
Pablo Aravena <paravena74@gmail.com>
2019-01-10 21:13:21 -0300

    first commit

---
+++ lyrical-graphql/client/components/LyricCreate.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+class LyricCreate extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { content: '' };
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        this.props.mutate({
+            variables: {
+                content: this.state.content,
+                songId: this.props.songId
+            }
+        }).then(() => this.setState({ content: '' }));
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit.bind(this)}>
+                <label>Add a Lyric</label>
+                <input type="text"
+                       onChange={(event) => this.setState({ content: event.target.value })}
+                       value={this.state.content}
+                />
+            </form>
+        );
+    }
+}
+
+const mutation = gql`
+    mutation AddLyricToSong($content:String, $songId:ID) {
+      addLyricToSong(content: $content, songId: $songId) {
+        id
+        lyrics {
+          id
+          content
+          likes
+        }
+      }
+    }
+`;
+export default graphql(mutation)(LyricCreate);
