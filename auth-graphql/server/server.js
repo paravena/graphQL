56ce113392177bc5ab984dc4d088e314a0f79349
Pablo Aravena <paravena@homeemergency.com>
2019-01-18 18:33:58 -0300

    graphql schema is finished

--- auth-graphql/server/server.js
+++ auth-graphql/server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://admin:admin123@ds161104.mlab.com:61104/auth';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
