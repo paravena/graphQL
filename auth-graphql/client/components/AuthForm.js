486d18050d1c53b6c04561aa81daa50f84ead621
Pablo Aravena <paravena74@gmail.com>
2019-01-19 18:40:31 -0300

    adding signin and login forms

---
+++ auth-graphql/client/components/AuthForm.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class AuthForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { email: '', password: '' };
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        this.props.onSubmit(this.state)
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <form className="col s6" onSubmit={this.onSubmit.bind(this)}>
+                    <div className="input-field">
+                        <input type="text" placeholder="Email"
+                               value={this.state.email}
+                               onChange={(e) => this.setState({ email: e.target.value })}/>
+                    </div>
+                    <div className="input-field">
+                        <input type="password" placeholder="Password"
+                               value={this.state.password}
+                               onChange={(e) => this.setState({ password: e.target.value })}/>
+                    </div>
+
+                    <div className="errors">
+                        {this.props.errors.map(error => <div key={error}>{error}</div>)}
+                    </div>
+
+                    <button className="btn">Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default AuthForm;
---
+++ auth-graphql/client/components/LoginForm.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import AuthForm from './AuthForm';
+import mutation from '../mutations/Login';
+import query from '../queries/CurrentUser';
+
+import { graphql } from "react-apollo";
+
+class LoginForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { errors: [] };
+    }
+
+    onSubmit({ email, password }) {
+        this.props.mutate({
+            variables: { email, password },
+            refetchQueries: [{ query }]
+        }).catch(res => {
+            const errors = res.graphQLErrors.map(error => error.message);
+            this.setState({ errors })
+        });
+    }
+
+    render() {
+        return (
+           <div>
+               <h3>Login</h3>
+               <AuthForm onSubmit={this.onSubmit.bind(this)}
+                         errors={this.state.errors}
+               />
+           </div>
+        );
+    }
+}
+
+export default graphql(mutation)(LoginForm);
